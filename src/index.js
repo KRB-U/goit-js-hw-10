d42e445d8789ff39f24f651d04e31cd6adc1bd21
unknown <rubikonspace@ukr.net>
2023-08-24 21:54:26 +0300

    dd

--- src/index.js
+++ src/index.js
@@ -5,34 +5,24 @@ const elements = {
   error: document.querySelector('.error'),
 };
 
-// import { fetchBreeds } from './js/cat-api';
-
-// const headers = new Headers({
-//   'Content-Type': 'application/json',
-//   'X-Custom-Header': 'custom value',
-//   breed_ids: '1',
-// });
-
 axios.defaults.headers.common['x-api-key'] =
   'live_NzqVk2Lk8XZBAri0CPIevLCyJd9M5NVP4r6FWzM4LBnTdRZi4cQ84F5TFDeRoxV2';
 
 function fetchBreeds() {
   return axios
     .get(`https://api.thecatapi.com/v1/breeds`)
-    .then(function (response) {
-      console.log(response.data);
-      // обработка успешного запроса
-      // console.log(response);
-    })
+    .then(resp => resp.data)
     .catch(function (error) {
-      // обработка ошибки
-      // console.log(error);
-    })
-    .finally(function () {
-      // выполняется всегда
+      console.log(error);
     });
+  // .finally(function () {
+  //   // выполняется всегда
+  // });
 }
-fetchBreeds().then(resp => {});
+
+fetchBreeds().then(resp => {
+  elements.list.innerHTML = createMarkup(resp);
+});
 
 function createMarkup(arr) {
   return arr
