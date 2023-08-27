ca5399a99322f2853aa8203da1d5b6eb4e45f2ca
unknown <rubikonspace@ukr.net>
2023-08-27 21:58:59 +0300

    fix

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,34 +1,34 @@
 import axios from 'axios';
 import { elements } from '.';
-import { Notify } from 'notiflix';
+// import { Notify } from 'notiflix';
+import { Report } from 'notiflix/build/notiflix-report-aio';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_NzqVk2Lk8XZBAri0CPIevLCyJd9M5NVP4r6FWzM4LBnTdRZi4cQ84F5TFDeRoxV2';
 
 function fetchBreeds() {
-  return axios
-    .get(`https://api.thecatapi.com/v1/breeds`)
-    .then(resp => resp.data)
-    .catch(function (error) {
-      Notify.failure(error.message);
-    });
+  return axios.get(`https://api.thecatapi.com/v1/breeds`);
+  // .then(resp => resp.data)
+  // .catch(function (error) {
+  //   // Notify.failure(error.message);
+  // });
   // .finally(function () {
   // });
 }
 
 function fetchCatByBreed(breedId) {
-  return axios
-    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-    .then(response => {
-      console.log(response);
-      if (response.data.length === 0) {
-        throw error;
-      }
-      return response.data;
-    })
-    .catch(err => {
-      Notify.failure(err.message);
-    });
+  return axios.get(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+  );
+  // .then(response => response.data);
+  // .catch(err => {
+  //   // Notify.failure(err.message);
+  // Report.failure(
+  //   'Notiflix Failure',
+  //   '"Oops! Something went wrong! Try reloading the page!"',
+  //   'Okay'
+  // );
+  // });
 }
 
 export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
+import { Report } from 'notiflix/build/notiflix-report-aio';
 
 export const elements = {
   list: document.querySelector('.breed-select'),
@@ -14,12 +15,18 @@ export const elements = {
 
 fetchBreeds()
   .then(resp => {
-    elements.list.innerHTML = createMarkup(resp);
+    elements.list.innerHTML = createMarkup(resp.data);
     elements.loader.style.display = 'none';
     elements.list.style.display = 'block';
   })
   .catch(err => {
     elements.error.style.display = 'block';
+    // Notify.failure(err.message);
+    Report.failure(
+      'Notiflix Failure',
+      '"Oops! Something went wrong! Try reloading the page!"',
+      'Okay'
+    );
   });
 
 function createMarkup(arr) {
@@ -38,8 +45,11 @@ function onChange(evt) {
   console.log(catId);
   fetchCatByBreed(catId)
     .then(resp => {
+      if (resp.data.length === 0) {
+        throw error;
+      }
       // console.log(resp);
-      createMarkupCats(resp);
+      createMarkupCats(resp.data);
       elements.catInfo.style.display = 'flex';
       elements.loader.style.display = 'none';
       elements.error.style.display = 'none';
@@ -47,6 +57,11 @@ function onChange(evt) {
     .catch(err => {
       elements.error.style.display = 'block';
       elements.loader.style.display = 'none';
+      Report.failure(
+        'Notiflix Failure',
+        '"Oops! Something went wrong! Try reloading the page!"',
+        'Okay'
+      );
     });
 }
 
